948608b2a2ef13345103f673ca4711f3d2a3d73b
Amirreza-Asp <m.senator.6247@gmail.com>
2023-02-28 00:52:04 +0330

    Update agent.ts

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -28,18 +28,17 @@ const sleep = (delay: number) => {
 
 axios.interceptors.request.use((config) => {
   const token = store.commonStore.token;
-  // config.headers = {
-  //   ...config.headers,
-  //   "Access-Control-Allow-Origin": "*",
-  //   "Access-Control-Allow-Methods": "*",
-  //   "Access-Control-Allow-Headers": "*",
-  // };
-  // if (token) {
-  //   config.headers = {
-  //     ...config.headers,
-  //     Authorization: `Bearer ${token}`,
-  //   };
-  // }
+  config.headers = {
+    ...config.headers,
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  };
+  if (token) {
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
   return config;
 });
 
