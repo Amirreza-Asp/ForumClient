655788f447c5491ee3cebfd7d7ea0002a902844b
Amirreza-Asp <m.senator.6247@gmail.com>
2023-03-13 23:59:39 +0330

    s;ldkas;dk

--- src/features/home/HomeProfile.tsx
+++ src/features/home/HomeProfile.tsx
@@ -1,48 +1,48 @@
-import React from 'react'
+import React from "react";
 
 export default function HomeProfile() {
   return (
-    <div className="profile">
-          <div className="profile-box">
-            <h2 className="profile-box-title">Complete Your Profile</h2>
+    <div className="profiles">
+      <div className="profiles-box">
+        <h2 className="profiles-box-title">Complete Your Profile</h2>
 
-            <ul className="profile-box-steps">
-              <li className="profile-box-steps-item">
-                <div>
-                  <span className="profile-box-steps-item-check"></span>
-                  <p className="profile-box-steps-item-para">
-                    General Information
-                  </p>
-                </div>
-                <span className="profile-box-steps-item-number">5/6</span>
-              </li>
-              <li className="profile-box-steps-item">
-                <div>
-                  <span className="profile-box-steps-item-check"></span>
-                  <p className="profile-box-steps-item-para">Work Experience</p>
-                </div>
-                <span className="profile-box-steps-item-number">1/3</span>
-              </li>
-              <li className="profile-box-steps-item">
-                <div>
-                  <span className="profile-box-steps-item-check check">
-                    <i className="fa fa-check"></i>
-                  </span>
-                  <p className="profile-box-steps-item-para">Profile Photo</p>
-                </div>
-                <span className="profile-box-steps-item-number check">1/1</span>
-              </li>
-              <li className="profile-box-steps-item">
-                <div>
-                  <span className="profile-box-steps-item-check check">
-                    <i className="fa fa-check"></i>
-                  </span>
-                  <p className="profile-box-steps-item-para">Cover Photo</p>
-                </div>
-                <span className="profile-box-steps-item-number check">1/1</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-  )
+        <ul className="profiles-box-steps">
+          <li className="profiles-box-steps-item">
+            <div>
+              <span className="profiles-box-steps-item-check"></span>
+              <p className="profiles-box-steps-item-para">
+                General Information
+              </p>
+            </div>
+            <span className="profiles-box-steps-item-number">5/6</span>
+          </li>
+          <li className="profiles-box-steps-item">
+            <div>
+              <span className="profiles-box-steps-item-check"></span>
+              <p className="profiles-box-steps-item-para">Work Experience</p>
+            </div>
+            <span className="profiles-box-steps-item-number">1/3</span>
+          </li>
+          <li className="profiles-box-steps-item">
+            <div>
+              <span className="profiles-box-steps-item-check check">
+                <i className="fa fa-check"></i>
+              </span>
+              <p className="profiles-box-steps-item-para">Profile Photo</p>
+            </div>
+            <span className="profiles-box-steps-item-number check">1/1</span>
+          </li>
+          <li className="profiles-box-steps-item">
+            <div>
+              <span className="profiles-box-steps-item-check check">
+                <i className="fa fa-check"></i>
+              </span>
+              <p className="profiles-box-steps-item-para">Cover Photo</p>
+            </div>
+            <span className="profiles-box-steps-item-number check">1/1</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
